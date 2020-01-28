085127f89a8a6c8f14e3d34b3e4f1b062ce6d59f
Bohdan Pohasii <bogdanpogasiy@gmail.com>
2020-01-28 11:25:13 +0200

    fix

--- public/js/base.js
+++ public/js/base.js
@@ -1,24 +1,3 @@
-console.log('from client js');
-
-fetch('http://puzzle.mead.io/puzzle')
-  .then(response => {
-    response.json().then(data => {
-      console.log(data);
-    })
-  });
-
-
-fetch('http://localhost:3000/whether?city=Boston')
-  .then(response => {
-    response.json().then(data => {
-      if (data.error) {
-        console.log(data.error);
-      } else {
-        console.log(data);
-      }
-    })
-  });
-
 const cityForm = document.querySelector('#city-form');
 const cityField = document.querySelector('input[name="city"]');
 const apiErrorField = document.querySelector('#api-error');
@@ -37,9 +16,12 @@ function loadWhether(e) {
           apiErrorField.textContent = data.error;
         } else {
           console.log(data);
-          apiSuccessField.textContent = data.toString();
+          apiSuccessField.textContent = JSON.stringify(data);
         }
       })
+    })
+    .catch(error => {
+      apiErrorField.textContent = data.error;
     });
 }
 
